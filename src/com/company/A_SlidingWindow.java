3b4933436e760fa9120208206bcc0309a82f5c53
Elda Moudy <elda@moudy.net>
2020-07-02 15:21:42 -0700

    findMaxSumBookSolution and nanotime

--- src/com/company/A_SlidingWindow.java
+++ src/com/company/A_SlidingWindow.java
@@ -12,24 +12,45 @@ public class A_SlidingWindow {
         //Explanation: Subarray with maximum sum is [5, 1, 3].
 
         public static int findMaxSumSubArray(int k, int[] arr) {
+            long startTime = System.nanoTime();
             int windowStart = 0;
             int windowSum = 0;
             int windowMaxSum = 0;
 
             for(int i=0; i <= arr.length-1; i++) {
                 windowSum += arr[i];
-                System.out.println("windowStart: " + windowStart);
                 if(i >= k-1){
                     if (windowSum >= windowMaxSum) {
                         windowMaxSum = windowSum;
-                        System.out.println("windowMaxSum " + windowMaxSum);
                     }
                     windowSum -= arr[windowStart];
                     windowStart++;
-                    //System.out.println("windowStart after: " + windowStart);
                 }
             }
+
+            long stopTime = System.nanoTime();
+            long elapsedTime = stopTime - startTime;
+            System.out.println("elapsedTime findMaxSumSubArray: " + elapsedTime);
             return windowMaxSum;
+        }
+
+        public static int findMaxSumSubArrayBookSolution(int k, int[] arr) {
+            long startTime = System.nanoTime();
+            int maxSum = 0;
+            int windowSum;
+
+            for (int i = 0; i <= arr.length - k; i++) {
+                windowSum = 0;
+                for (int j = i; j < i + k; j++) {
+                    windowSum += arr[j];
+                }
+                maxSum = Math.max(maxSum, windowSum);
+            }
+
+            long stopTime = System.nanoTime();
+            long elapsedTime = stopTime - startTime;
+            System.out.println("elapsedTime findMaxSumSubArrayBookSolution: " + elapsedTime);
+            return maxSum;
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,7 @@ public class Main {
         //Max Sum = 9
         //Explanation: Subarray with maximum sum is [5, 1, 3].
 
-        int[] arr = {2, 1, 5, 1, 3, 2};
-        int k = 3;
-        System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArray(k, arr));
+        System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[] {2, 1, 5, 1, 3, 2}));
+        System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArrayBookSolution(3, new int[] {2, 1, 5, 1, 3, 2}));
     }
 }
