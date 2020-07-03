b4d9c95c4219cd30002ff951236225128b937cef
Elda Moudy <elda@moudy.net>
2020-07-02 22:04:35 -0700

    Added Smallest Subarray solution

--- src/com/company/A_SlidingWindow.java
+++ src/com/company/A_SlidingWindow.java
@@ -2,10 +2,9 @@ package com.company;
 
 public class A_SlidingWindow {
     static class MaxSumSubArrayOfSizeK {
-        //Problem Statement
-        //Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+        //Maximum Sum Subarray of Size K
 
-        //Example1
+        //Problem Statement: Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
         int[] arr = {2, 1, 5, 1, 3, 2};
         int k = 3;
         //Max Sum = 9
@@ -14,30 +13,29 @@ public class A_SlidingWindow {
         public static int findMaxSumSubArray(int k, int[] arr) {
             //Time complexity O(n)
             //Space complexity O(1)
-            long startTime = System.nanoTime();
+//            long startTime = System.nanoTime();
             int windowStart = 0;
             int windowSum = 0;
             int maxSum = 0;
 
-            for(int i=0; i <= arr.length-1; i++) {
+            for (int i = 0; i <= arr.length - 1; i++) {
                 windowSum += arr[i];
-                if(i >= k-1){
+                if (i >= k - 1) {
                     maxSum = Math.max(maxSum, windowSum);
                     windowSum -= arr[windowStart];
                     windowStart++;
                 }
             }
 
-            long stopTime = System.nanoTime();
-            long elapsedTime = stopTime - startTime;
-            System.out.println("elapsedTime findMaxSumSubArray: " + elapsedTime);
+//            long stopTime = System.nanoTime();
+//            long elapsedTime = stopTime - startTime;
+//            System.out.println("elapsedTime findMaxSumSubArray: " + elapsedTime);
             return maxSum;
         }
 
         public static int findMaxSumSubArrayBookSolution1(int k, int[] arr) {
             //Time complexity O(n * k)
             //Space complexity O(1)
-            long startTime = System.nanoTime();
             int maxSum = 0;
             int windowSum;
 
@@ -48,10 +46,6 @@ public class A_SlidingWindow {
                 }
                 maxSum = Math.max(maxSum, windowSum);
             }
-
-            long stopTime = System.nanoTime();
-            long elapsedTime = stopTime - startTime;
-            System.out.println("elapsedTime findMaxSumSubArrayBookSolution: " + elapsedTime);
             return maxSum;
         }
 
@@ -72,6 +66,67 @@ public class A_SlidingWindow {
                 }
             }
             return maxSum;
+        }
+    }
+
+    static class MinSizeSubArraySum {
+    //Smallest Subarray with a given sum
+
+//        Problem Statement: Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+//        Input: [2, 1, 5, 2, 3, 2], S=7
+//        Output: 2
+//        Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+        public static int findMinSubArray(int s, int[] arr) {
+            //Time complexity O(n)
+            //Space complexity O(1)
+            int minSubArraySize = Integer.MAX_VALUE;
+            int windowSum = 0;
+            int windowStart = 0;
+
+            for (int windowEnd = 0; windowEnd < arr.length; windowEnd++){
+                windowSum += arr[windowEnd];
+
+                while (windowSum >= s){
+                    int tempArraySize = windowEnd - windowStart + 1;
+                    minSubArraySize = Math.min(tempArraySize, minSubArraySize);
+                    windowSum -= arr[windowStart];
+                    windowStart++;
+                }
+            }
+            if (minSubArraySize == Integer.MAX_VALUE) return -1;
+            return minSubArraySize;
+        }
+
+        public static int findMinSubArrayBookSolution(int S, int[] arr) {
+            int windowSum = 0;
+            int minLength = Integer.MAX_VALUE;
+            int windowStart = 0;
+
+            for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+                windowSum += arr[windowEnd]; // add the next element
+                // shrink the window as small as possible until the 'windowSum' is smaller than 'S'
+                while (windowSum >= S) {
+                    minLength = Math.min(minLength, windowEnd - windowStart + 1);
+                    windowSum -= arr[windowStart]; // subtract the element going out
+                    windowStart++; // slide the window ahead
+                }
+            }
+            return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        }
+    }
+
+
+    static class LongestSubstringKDistinct {
+        //Longest Substring with K Distinct Characters
+
+//        Given a string, find the length of the longest substring in it with no more than K distinct characters.
+//        Input: String="araaci", K=2
+//        Output: 4
+//        Explanation: The longest substring with no more than '2' distinct characters is "araa".
+        static int findLength(String str, int k) {
+            // TODO: Write your code here
+            return -1;
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,19 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        //A_SlidingWindow
-        //Example1
-        //Max Sum = 9
-        //Explanation: Subarray with maximum sum is [5, 1, 3].
+        /*
+        A_SlidingWindow
+        */
 
-        System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[] {2, 1, 5, 1, 3, 2}));
+        //Example1
+        //Explanation: Subarray with maximum sum is [5, 1, 3].
+        //Max Sum = 9
+        //System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArray(3, new int[] {2, 1, 5, 1, 3, 2}));
         //System.out.println(A_SlidingWindow.MaxSumSubArrayOfSizeK.findMaxSumSubArrayBookSolution(3, new int[] {2, 1, 5, 1, 3, 2}));
+
+        //Example2
+        //Explanation: Given an array of positive numbers and a positive number ‘S’, find the length of the
+        // smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+        System.out.println(A_SlidingWindow.MinSizeSubArraySum.findMinSubArray(7, new int[] {2, 1, 5, 2, 8}));
     }
 }
